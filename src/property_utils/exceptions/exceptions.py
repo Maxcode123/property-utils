47a91445249b6eb2b9f2107769d279a215ef5046
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-02-12 18:35:56 +0200

    Add exceptions dir and file

---
+++ src/property_utils/exceptions/exceptions.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+class PropertyUtilsException(Exception):
+    """
+    Base exception for property-utils library. Any exception raised from the
+    modules of this library should inherit from this class.
+    """
+
+    description: str
+
+    def __init__(self, msg: Optional[str] = None, *args) -> None:
+        if msg is None:
+            msg = self.description
+        super().__init__(msg, *args)
+
+
+class InvalidDescriptorBinaryOperation(PropertyUtilsException):
+    description = "invalid binary operation between descriptors. "
+
+
+class WrongUnitDescriptorType(PropertyUtilsException):
+    description = "got a wrong unit descriptor type. "
+
+
+class InvalidUnitConversion(PropertyUtilsException):
+    description = (
+        "conversion to invalid physical property unit (e.g. from m^2 to cm^3 or from K"
+        " to bar). "
+    )
+
+
+class UndefinedConverter(PropertyUtilsException):
+    description = "a converter has not been defined for a given generic descriptor. "
