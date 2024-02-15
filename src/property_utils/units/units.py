a806607b8bc26f90e270abcea534acf6a53c33d1
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-02-15 18:41:08 +0200

    Introduce basic measurement units

---
+++ src/property_utils/units/units.py
@@ -0,0 +1,68 @@
+from property_utils.units.descriptors import (
+    MeasurementUnit,
+    AliasMeasurementUnit,
+)
+
+
+class NonDimensionalUnit(MeasurementUnit):
+    """
+    This enum is used to denote physical quantities that do not have a unit of
+    measurement.
+    """
+
+    NON_DIMENSIONAL = ""
+
+
+class TemperatureUnit(MeasurementUnit):
+    CELCIUS = "°C"
+    KELVIN = "K"
+    FAHRENHEIT = "°F"
+    RANKINE = "°R"
+
+
+class LengthUnit(MeasurementUnit):
+    MILLI_METER = "mm"
+    CENTI_METER = "cm"
+    METER = "m"
+    KILO_METER = "km"
+    INCH = "in"
+    FOOT = "ft"
+
+
+class MassUnit(MeasurementUnit):
+    MILLI_GRAM = "mg"
+    GRAM = "g"
+    KILO_GRAM = "kg"
+    METRIC_TONNE = "MT"
+    POUND = "lb"
+
+
+class AmountUnit(MeasurementUnit):
+    MOL = "mol"
+    KILO_MOL = "kmol"
+
+
+class TimeUnit(MeasurementUnit):
+    MILLI_SECOND = "ms"
+    SECOND = "s"
+    MINUTE = "min"
+    HOUR = "hr"
+    DAY = "d"
+
+
+class PressureUnit(AliasMeasurementUnit):
+    MILLI_BAR = "mbar"
+    BAR = "bar"
+    PSI = "psi"
+    PASCAL = "Pa"
+    KILO_PASCAL = "kPa"
+
+
+class EnergyUnit(AliasMeasurementUnit):
+    JOULE = "J"
+    KILO_JOULE = "kJ"
+    MEGA_JOULE = "MJ"
+    GIGA_JOULE = "GJ"
+    CALORIE = "cal"
+    KILO_CALORIE = "kcal"
+    BTU = "Btu"
