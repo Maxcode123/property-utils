8513398dcdda188309af4bcbec833008cb907d7c
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-02-22 19:34:08 +0200

    Introduce more units

--- src/property_utils/units/units.py
+++ src/property_utils/units/units.py
@@ -30,6 +30,15 @@ class TemperatureUnit(MeasurementUnit):
         return cls.KELVIN
 
 
+class AbsoluteTemperatureUnit(MeasurementUnit):
+    KELVIN = "K"
+    RANKINE = "°R"
+
+    @classmethod
+    def si(cls) -> "AbsoluteTemperatureUnit":
+        return cls.KELVIN
+
+
 class LengthUnit(MeasurementUnit):
     MILLI_METER = "mm"
     CENTI_METER = "cm"
@@ -76,12 +85,36 @@ class TimeUnit(MeasurementUnit):
         return cls.SECOND
 
 
+class ElectricCurrentUnit(MeasurementUnit):
+    MILLI_AMPERE = "mA"
+    AMPERE = "A"
+    KILO_AMPERE = "kA"
+
+    @classmethod
+    def si(cls) -> "ElectricCurrentUnit":
+        return cls.AMPERE
+
+
+class ForceUnit(AliasMeasurementUnit):
+    NEWTON = "N"
+    DYNE = "dyn"
+
+    @classmethod
+    def aliased_generic_descriptor(cls) -> GenericCompositeDimension:
+        return MassUnit * LengthUnit / (TimeUnit**2)
+
+    @classmethod
+    def si(cls) -> "ForceUnit":
+        return cls.NEWTON
+
+
 class PressureUnit(AliasMeasurementUnit):
     MILLI_BAR = "mbar"
     BAR = "bar"
     PSI = "psi"
     PASCAL = "Pa"
     KILO_PASCAL = "kPa"
+    MEGA_PASCAL = "MPa"
 
     @classmethod
     def aliased_generic_descriptor(cls) -> GenericCompositeDimension:
@@ -108,3 +141,18 @@ class EnergyUnit(AliasMeasurementUnit):
     @classmethod
     def si(cls) -> "EnergyUnit":
         return cls.JOULE
+
+
+class PowerUnit(AliasMeasurementUnit):
+    WATT = "W"
+    KILO_WATT = "kW"
+    MEGA_WATT = "MW"
+    GIGA_WATT = "GW"
+
+    @classmethod
+    def aliased_generic_descriptor(cls) -> GenericCompositeDimension:
+        return MassUnit * (LengthUnit**2) / (TimeUnit**3)
+
+    @classmethod
+    def si(cls) -> "PowerUnit":
+        return cls.WATT
