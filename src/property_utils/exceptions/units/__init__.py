2358e5b47a2b3c08502606b1b44224af52420e01
Maximos Nikiforakis <nikiforos@live.co.uk>
2024-03-21 08:41:26 +0200

    Exception modules __init__ imports

---
+++ src/property_utils/exceptions/units/__init__.py
@@ -0,0 +1,12 @@
+from property_utils.exceptions.units.converter_types import (
+    UndefinedConverterError,
+    UnsupportedConverterError,
+    ConverterDependenciesError,
+    UnitConversionError,
+    ConversionFunctionError,
+)
+from property_utils.exceptions.units.descriptors import (
+    DescriptorBinaryOperationError,
+    DescriptorExponentError,
+    UnitDescriptorTypeError,
+)
